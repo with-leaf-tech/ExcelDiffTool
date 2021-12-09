ee147e0525e85e650a90531c8d180af393cb5fa3
with-leaf-tech <ktsutsumi@par.odn.ne.jp>
2021-12-09 10:52:21 +0900

    プロジェクト ファイルを追加します。

---
+++ ExcelDiffTool/ExcelDiffForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ExcelDiffTool {
+    partial class ExcelDiffForm {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 19);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "参照";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(453, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(119, 57);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(48, 23);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "計算式";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "値";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(579, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 51);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "差分検出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox3.Location = new System.Drawing.Point(0, 88);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(1019, 362);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(687, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 7;
+            // 
+            // ExcelDiffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ExcelDiffForm";
+            this.Text = "ExcelDiffTool";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ExcelDiffTool/ExcelDiffForm.cs
@@ -0,0 +1,181 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelDiffTool {
+    public partial class ExcelDiffForm : Form {
+        public ExcelDiffForm() {
+            InitializeComponent();
+            textBox3.Height = this.Height - 110;
+            radioButton1.Checked = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            textBox3.Height = this.Height - 110;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            openFileDialog(textBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            openFileDialog(textBox2);
+        }
+
+        private void openFileDialog(TextBox textbox) {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //ofd.FileName = "default.html";
+            //ofd.InitialDirectory = @"C:\";
+            //ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            ofd.Filter = @"Excel ファイル|*.xls;*.xlsx;*.xlsm|全てのファイル|*.*";
+            ofd.FilterIndex = 1;
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                textbox.Text = ofd.FileName;
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e) {
+            textBox3.Text = "";
+            await Task.Run(() => checkFiles(textBox1.Text, textBox2.Text));
+        }
+
+        private void checkFiles(string file1, string file2) {
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 4;
+
+
+            StringBuilder sb = new StringBuilder();
+
+            if (!File.Exists(file1)) {
+                MessageBox.Show("ファイル1が存在しません");
+                return;
+            }
+            if (!File.Exists(file2)) {
+                MessageBox.Show("ファイル2が存在しません");
+                return;
+            }
+            if (file1 == file2) {
+                MessageBox.Show("ファイル1とファイル2が同一です");
+                return;
+            }
+            updateStatus(false);
+
+
+
+
+            Workbook workbook1 = new Workbook();
+            Workbook workbook2 = new Workbook();
+
+            workbook1.LoadFromFile(file1);
+            workbook2.LoadFromFile(file2);
+
+            if (workbook1.Worksheets.Count != workbook2.Worksheets.Count) {
+                appendText("シート数が違います\tファイル1=" + workbook1.Worksheets.Count + "\tファイル2=" + workbook2.Worksheets.Count + Environment.NewLine);
+                //sb.Append("シート数が違います\tファイル1=" + workbook1.Worksheets.Count + "\tファイル2=" + workbook2.Worksheets.Count + Environment.NewLine);
+                //textBox3.Text = sb.ToString();
+                updateStatus(true);
+                return;
+            }
+
+            if (!radioButton1.Checked) {
+                appendText("シート名\tセル\t計算式1\t計算式2" + Environment.NewLine);
+            }
+            else {
+                appendText("シート名\tセル\t値1\t値2" + Environment.NewLine);
+            }
+
+            for (int i = 0; i < workbook1.Worksheets.Count; i++) {
+                Worksheet sheet1 = workbook1.Worksheets[i];
+                Worksheet sheet2 = workbook2.Worksheets[i];
+
+                int maxRows = sheet1.Rows.Length;
+                int maxColumns = sheet1.Columns.Length;
+
+                if (sheet1.Columns.Length != sheet2.Columns.Length || sheet1.Rows.Length != sheet2.Rows.Length) {
+                    appendText("レイアウトが違います\tシート=[" + sheet1.Name + "]\t" + sheet1.Columns.Length + "×" + sheet1.Rows.Length + "\t" + sheet1.Columns.Length + "×" + sheet1.Rows.Length + Environment.NewLine);
+                    //sb.Append("レイアウトが違います シート=[" + sheet1.Name + "]\tファイル1=[" + sheet1.Columns.Length + "×" + sheet1.Rows.Length + "]\tファイル2=[" + sheet1.Columns.Length + "×" + sheet1.Rows.Length + "]" + Environment.NewLine);
+                    //textBox3.Text = sb.ToString();
+                    updateStatus(true);
+                    return;
+                }
+
+                //CellRange aa = sheet1.Range[0, 0, maxRows, maxColumns];
+                //aa.Cells[]
+
+                for (int j = 1; j < maxRows+1; j++) {
+                    this.Invoke((MethodInvoker)delegate {
+                        label1.Text = "チェック中...シート[" + sheet1.Name + "]" + Environment.NewLine + "maxRows=" + maxRows + " Row =" + j;
+                    });
+                    //Parallel.For(1, maxColumns + 1, options, k => {
+                    for (int k = 1; k < maxColumns+1; k++) {
+
+                        CellRange cell1 = sheet1.Range[j, k];
+                        CellRange cell2 = sheet2.Range[j, k];
+                        if (!radioButton1.Checked) {
+                            if (cell1.Formula != cell2.Formula) {
+                                appendText(sheet1.Name + "\t" + cell1.RangeAddressLocal + "\t" + cell1.Formula + "\t" + cell2.Formula + Environment.NewLine);
+                                //sb.Append("計算式が違います\tシート[" + sheet1.Name + "]" + "\tファイル1[" + j + "," + k + "]=" + cell1.Formula + "\tファイル2[" + j + "," + k + "]=" + cell2.Formula + Environment.NewLine);
+                            }
+                        }
+                        else {
+                            if (cell1.DisplayedText != cell2.DisplayedText) {
+                                appendText(sheet1.Name + "\t" + cell1.RangeAddressLocal + "\t" + cell1.DisplayedText + "\t" + cell2.DisplayedText + Environment.NewLine);
+                                //sb.Append("値が違います\tシート[" + sheet1.Name + "]" + "\tファイル1[" + j + "," + k + "]=" + cell1.Formula + "\tファイル2[" + j + "," + k + "]=" + cell2.Formula + Environment.NewLine);
+                            }
+                        }
+                        }
+                    //});
+                }
+            }
+
+            updateStatus(true);
+            this.Invoke((MethodInvoker)delegate {
+                label1.Text = "";
+            });
+            MessageBox.Show("差分検出が終了しました");
+
+
+            //textBox3.Text = sb.ToString();
+        }
+
+        private void appendText(string text) {
+            this.Invoke((MethodInvoker)delegate {
+                textBox3.Text += text;
+            });
+        }
+
+        private void updateStatus(bool status) {
+            this.Invoke((MethodInvoker)delegate {
+                textBox1.Enabled = status;
+                textBox2.Enabled = status;
+                button1.Enabled = status;
+                button2.Enabled = status;
+                button3.Enabled = status;
+                radioButton1.Enabled = status;
+                radioButton2.Enabled = status;
+
+            });
+        }
+
+    }
+}
